76b9b0a48d190d355f9fc1895acf9d5cd636b6f5
Joshua Harms <joshua@nozzlegear.com>
2016-12-21 14:48:24 -0600

    Bugfix: Default to browser's own timezone.

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { yellow } from "chalk";
 
 declare const require;
 let util: { inspect: (input: string, options?: { colors?: boolean; }) => string };
+let inBrowserTimezoneWarningShown = false;
 
 if (isBrowser) {
     util = { inspect: (input, options) => input };
@@ -16,7 +17,17 @@ export const defaults = {
 }
 
 export function inspect(...args) {
-    const time = yellow(new Date().toLocaleTimeString(defaults.locale, { timeZone: defaults.timeZone, timeZoneName: "short" }));
+    if (isBrowser && !inBrowserTimezoneWarningShown) {
+        inBrowserTimezoneWarningShown = true;   
+
+        // Ref: https://stackoverflow.com/q/30377607
+        console.warn("logspect warning: Timezones are not supported in all browsers. Timestamp will default to the browser's current timezone.");
+    }
+
+    const time = yellow(new Date().toLocaleTimeString(defaults.locale, { 
+        timeZone: isBrowser ? undefined : defaults.timeZone, 
+        timeZoneName: "short" 
+    }));
 
     console.log(time, ...args.map(a => typeof(a) === "string" ? a : util.inspect(a, { colors: true })));
 }
