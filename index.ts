b7a1bc87a6fc7d8d7069819d8142316897eafa5c
Joshua Harms <joshua@nozzlegear.com>
2017-09-20 15:11:49 -0500

    Default to plain console.log when args cannot be logged (IE11 with babel polyfill for example)

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
-import isBrowser from "is-in-browser";
-import { yellow } from "chalk";
+import isBrowser from 'is-in-browser';
+import { yellow } from 'chalk';
 
 declare const require;
 let util: { inspect: (input: string, options?: { colors?: boolean; }) => string };
 let inBrowserTimezoneWarningShown = false;
+let warnedOnError = false;
 
 if (isBrowser) {
     util = { inspect: (input, options) => input };
@@ -17,19 +18,30 @@ export const defaults = {
 }
 
 export function inspect(...args) {
-    if (isBrowser && !inBrowserTimezoneWarningShown) {
-        inBrowserTimezoneWarningShown = true;   
+    try {
+        if (isBrowser && !inBrowserTimezoneWarningShown) {
+            inBrowserTimezoneWarningShown = true;
 
-        // Ref: https://stackoverflow.com/q/30377607
-        console.warn("logspect warning: Timezones are not supported in all browsers. Timestamp will default to the browser's current timezone.");
+            // Ref: https://stackoverflow.com/q/30377607
+            console.warn("logspect warning: Timezones are not supported in all browsers. Timestamp will default to the browser's current timezone.");
+        }
+
+        const time = yellow(new Date().toLocaleTimeString(defaults.locale, {
+            timeZone: isBrowser ? undefined : defaults.timeZone,
+            timeZoneName: "short"
+        }));
+
+        console.log(time, ...args.map(a => typeof (a) === "string" ? a : util.inspect(a, { colors: true })));
     }
+    catch (e) {
+        if (!warnedOnError) {
+            warnedOnError = true;
 
-    const time = yellow(new Date().toLocaleTimeString(defaults.locale, { 
-        timeZone: isBrowser ? undefined : defaults.timeZone, 
-        timeZoneName: "short" 
-    }));
+            console.error("Logspect error: could not log arguments. Defaulting to plain console.log.", e);
+        }
 
-    console.log(time, ...args.map(a => typeof(a) === "string" ? a : util.inspect(a, { colors: true })));
+        console.log(...args);
+    }
 }
 
 export default inspect;
